2309ec4488bea46a82cb01d42d9870a5d58d1827
adrenalin <borivoj.bradna@gmail.com>
2018-12-17 21:26:55 +0100

    VIP

--- Project_101/Project_101/Project_101.py
+++ Project_101/Project_101/Project_101.py
@@ -1,39 +1,68 @@
+#| USER |   PASSWORD  |
+#-----------------------
+#| bob  |     123     |
+#| ann  |    pass123  |
+#| mike | password123 |
+#| liz  |    pass123  |
+
+# our dictionary with data
+data = {
+    'bob': '123',
+    'ann': 'pass123',
+    'mike': 'password123',
+    'liz': 'pass123',
+            }
+
+# we want to ask user for username and password
+Username    = str(input('Please enter the username: '))
+Password    = str(input('Please enter the password: '))
+
+# two conditions for evaluating the inputs
+if data.get(Username) != Password:
+    print('Password or username is wrong')
+
+elif data.get(Username) == Password:
+    print('Permission granted')
+
+
+
 #'''
 #author = 
 #'''
-#TEXTS = ['''
-#Situated about 10 miles west of Kemmerer, 
-#Fossil Butte is a ruggedly impressive 
-#topographic feature that rises sharply 
-#some 1000 feet above Twin Creek Valley 
-#to an elevation of more than 7500 feet 
-#above sea level. The butte is located just 
-#north of US 30N and the Union Pacific Railroad, 
-#which traverse the valley. ''',
-
-#'''At the base of Fossil Butte are the bright 
-#red, purple, yellow and gray beds of the Wasatch 
-#Formation. Eroded portions of these horizontal 
-#beds slope gradually upward from the valley floor 
-#and steepen abruptly. Overlying them and extending 
-#to the top of the butte are the much steeper 
-#buff-to-white beds of the Green River Formation, 
-#which are about 300 feet thick.''',
-
-#'''The monument contains 8198 acres and protects 
-#a portion of the largest deposit of freshwater fish 
-#fossils in the world. The richest fossil fish deposits 
-#are found in multiple limestone layers, which lie some 
-#100 feet below the top of the butte. The fossils 
-#represent several varieties of perch, as well as 
-#other freshwater genera and herring similar to those 
-#in modern oceans. Other fish such as paddlefish, 
-#garpike and stingray are also present.'''
-#]
-
 
 # definice textu
-text = "Situated about 10 miles west of Kemmerer, Fossil Butte is a ruggedly impressive topographic feature that rises sharply some 1000 feet above Twin Creek Valley to an elevation of more than 7500 feet above sea level. The butte is located just north of US 30N and the Union Pacific Railroad, which traverse the valley."
+
+texts = ['''
+situated about 10 miles west of kemmerer, 
+fossil butte is a ruggedly impressive 
+topographic feature that rises sharply 
+some 1000 feet above twin creek valley 
+to an elevation of more than 7500 feet 
+above sea level. the butte is located just 
+north of us 30n and the union pacific railroad, 
+which traverse the valley.''',
+'''at the base of fossil butte are the bright 
+red, purple, yellow and gray beds of the wasatch 
+formation. eroded portions of these horizontal 
+beds slope gradually upward from the valley floor 
+and steepen abruptly. overlying them and extending 
+to the top of the butte are the much steeper 
+buff-to-white beds of the green river formation, 
+which are about 300 feet thick.''',
+'''the monument contains 8198 acres and protects 
+a portion of the largest deposit of freshwater fish 
+fossils in the world. the richest fossil fish deposits 
+are found in multiple limestone layers, which lie some 
+100 feet below the top of the butte. the fossils 
+represent several varieties of perch, as well as 
+other freshwater genera and herring similar to those 
+in modern oceans. other fish such as paddlefish, 
+garpike and stingray are also present.''']
+
+text_choose = int(input('Choose the text you want to analyze [1,2 or 3]: '))
+
+text = texts[text_choose - 1]
+
 #final_text = text.replace(',',' ')
 text_array = text.split(' ')
 words_count = len(text_array)
